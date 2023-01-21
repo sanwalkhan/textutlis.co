42d04ab0d215ef7450f41423cb02f2093be7353c
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2023-01-21 19:04:10 +0500

    Update Navbar.js

--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
@@ -42,3 +43,7 @@ export default function Navbar(props) {
     </div>
   )
 }
+
+
+Navbar.propTypes = {title : PropTypes.string,
+about : PropTypes.string} 
