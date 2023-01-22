60ce08ee44faccf6ad5a763b629274a4b8c9cb5b
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2023-01-22 18:31:40 +0500

    textforms.js
    adding some more functions

--- textutils/src/components/textForm.js
+++ textutils/src/components/textForm.js
@@ -1,14 +1,52 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 
 export default function TXextForm(props) {
+  const [text, settext] = useState("Enter text Here...!");
+
+  const handleClickUPC = () => {
+    let newtext = text.toUpperCase();
+    settext(newtext);
+  };
+
+  const handleClickLOC = ()=>{
+    let newtext = text.toLowerCase();
+    settext(newtext);
+  }
+
+  const handleonchange = (e) => {
+    settext(e.target.value);
+  };
   return (
     <div>
-    <h3>{props.heading}</h3>
+    <div className="container">
+      <h3>{props.heading}</h3>
 
       <div className="mb-3">
-        <textarea className="form-control" id="box" rows="8"></textarea>
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleonchange}
+          id="box"
+          rows="8"
+        ></textarea>
       </div>
-      <button className="btn btn-primary">Convert to Uppercase</button>
+      <button className="btn btn-primary mx-3" onClick={handleClickUPC}>
+        Convert to Uppercase
+      </button>
+      
+      <button className="btn btn-secondary mx-3" onClick={handleClickLOC}>
+        Convert to LowerCase
+      </button>
+    </div>
+    <div className="container my-3">
+    <h3>Your Text Summary</h3>
+    <p>{text.split(" ").length} Words</p>
+    <p>{text.length} Caracters</p>
+    <p>{0.008 * text.split(" ").length} Minutes to read </p>
+    <h4>Preview</h4>
+    <p>{text}</p>
+    </div>
+
     </div>
   );
 }
