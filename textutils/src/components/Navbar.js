79b1fd7012d28f7267355011ff6b01fafd84da20
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2023-01-22 13:58:43 +0500

    update

--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -4,38 +4,38 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
     <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" > {props.title} </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" > {props.title} </a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" >{props.Home}</a>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" >{props.Home}</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" >{props.about}</a>
+          <li className="nav-item">
+            <a className="nav-link" >{props.about}</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Services
             </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" >Action</a></li>
-              <li><a class="dropdown-item" >Another action</a></li>
-              <li><hr class="dropdown-divider"/></li>
-              <li><a class="dropdown-item" >Something else here</a></li>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a className="dropdown-item" >Action</a></li>
+              <li><a className="dropdown-item" >Another action</a></li>
+              <li><hr className="dropdown-divider"/></li>
+              <li><a className="dropdown-item" >Something else here</a></li>
             </ul>
           </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">_____</a>
+          <li className="nav-item">
+            <a className="nav-link disabled">_____</a>
           </li>
         </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button class="btn btn-outline-success" type="submit">Search</button>
+        <form className="d-flex">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button className="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
     </div>
--- textutils/src/components/textForm.js
+++ textutils/src/components/textForm.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React , {useState} from "react";
 
 export default function TXextForm(props) {
   return (
     <div>
     <h3>{props.heading}</h3>
 
-      <div class="mb-3">
+      <div className="mb-3">
         <textarea className="form-control" id="box" rows="8"></textarea>
       </div>
+      <button className="btn btn-primary">Convert to Uppercase</button>
     </div>
   );
 }
